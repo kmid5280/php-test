421076cc8748ba8c611bf48a4c82d3560f76c730
Kyle Middleton <kylemid321@gmail.com>
2019-04-19 14:27:50 -0600

    added php express support

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const phpExpress = require('php-express')({
+    binPath: 'php'
+})
+
+app.set('views', './views')
+app.engine('php', phpExpress.engine)
+app.set('view engine', 'php')
+app.all(/.+\.php$/, phpExpress.router)
 
 app.get('/', (req, res) => res.send('hello world'))
 
-app.listen(port, () => console.log(`App listening on port ${port}`))
+const server = app.listen(port, function() {
+    const host = server.address().address
+    const port = server.address().port;
+    console.log('PHPExpress app listening at http://%s:%s', host, port)
+})
